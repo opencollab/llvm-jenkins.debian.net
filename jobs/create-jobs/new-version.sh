594a73fde990fd0fa850e04c0533aabf6766c962
Sylvestre Ledru <sylvestre@debian.org>
2017-01-14 13:33:25 +0100

    give the command to update the sync jobs

--- jobs/create-jobs/new-version.sh
+++ jobs/create-jobs/new-version.sh
@@ -7,4 +7,10 @@ do
 	echo $d
 	sh create-new-job.sh $d $VERSION release_$VERSION_2 $VERSION
 # sh create-new-job.sh unstable 3.5 release_35 3.5
+	if test "$d" == "unstable"; then
+		d=""
+	else
+		d="-$d"
+	fi
+	echo "sed -i -e 's|llvm-toolchain$d-binaries,|llvm-toolchain$d-binaries, llvm-toolchain$d-$VERSION-binaries,|' ../llvm-toolchain$d-binaries-sync/config.xml"
 done
