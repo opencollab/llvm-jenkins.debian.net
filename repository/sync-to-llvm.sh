f2b873abbd0d7f9c7137c7359e4b14355627a1f2
Sylvestre Ledru <sylvestre@debian.org>
2017-01-22 17:02:55 +0100

    update of the sync script

--- repository/sync-to-llvm.sh
+++ repository/sync-to-llvm.sh
@@ -7,15 +7,16 @@ if test $# -ne 1; then
 fi
 
 REPOSITORY=$1
-TARGET=apt@llvm.org
-BASE_TARGETDIR=/home/apt
+TARGET=apt@apt-origin.llvm.org
+BASE_TARGETDIR=/data/apt/www
 BASE_LOCALDIR=/srv/repository
 if test ! -d $BASE_LOCALDIR/$REPOSITORY; then
 	echo "Cannot find directory $REPOSITORY"
 	exit 1
 fi
-
-time /usr/bin/rsync -v --stats --delete -r $BASE_LOCALDIR/$REPOSITORY $TARGET:$BASE_TARGETDIR/www/
+find $BASE_LOCALDIR -type d | xargs chmod 755
+find $BASE_LOCALDIR -type f ! -name sync-to-llvm.sh | xargs chmod 644
+time /usr/bin/rsync --delay-updates -v --stats --delete -r $BASE_LOCALDIR/$REPOSITORY $TARGET:$BASE_TARGETDIR/
 #ssh $TARGET mv $BASE_TARGETDIR/www/$REPOSITORY $BASE_TARGETDIR/www/$REPOSITORY.back
 #ssh $TARGET mv $BASE_TARGETDIR/tmp-repo/$REPOSITORY $BASE_TARGETDIR/www/$REPOSITORY
 #ssh $TARGET rm -rf $BASE_TARGETDIR/www/$REPOSITORY.back
