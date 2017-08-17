407b0ee728ea302e068fe5eb578542d1d61b9b28
Sylvestre Ledru <sylvestre@debian.org>
2017-08-17 23:33:16 +0200

    script to sync to llvm.org

--- sync-to-llvm.sh
+++ sync-to-llvm.sh
@@ -1,1 +1,23 @@
-/srv/repository/sync-to-llvm.sh
+#!/bin/bash -v
+
+if test $# -ne 1; then
+	echo "Wrong number of args."
+	echo "Syntax: $0 <repository>"
+	exit 1
+fi
+
+REPOSITORY=$1
+TARGET=apt@apt-origin.llvm.org
+BASE_TARGETDIR=/data/apt/www
+BASE_LOCALDIR=/srv/repository
+if test ! -d $BASE_LOCALDIR/$REPOSITORY; then
+	echo "Cannot find directory $REPOSITORY"
+	exit 1
+fi
+find $BASE_LOCALDIR -type d | xargs chmod 755
+find $BASE_LOCALDIR -type f ! -name sync-to-llvm.sh | xargs chmod 644
+time ssh $TARGET cp -R $BASE_TARGETDIR/$REPOSITORY $BASE_TARGETDIR/$REPOSITORY.back
+time /usr/bin/rsync --delay-updates --info=progress2 --block-size=131072 --protocol=29 --times --delete -v --stats -r $BASE_LOCALDIR/$REPOSITORY/* $TARGET:$BASE_TARGETDIR/$REPOSITORY.back/
+time ssh $TARGET mv $BASE_TARGETDIR/$REPOSITORY $BASE_TARGETDIR/$REPOSITORY.1
+time ssh $TARGET mv $BASE_TARGETDIR/$REPOSITORY.back $BASE_TARGETDIR/$REPOSITORY
+time ssh $TARGET rm -rf $BASE_TARGETDIR/$REPOSITORY.1
