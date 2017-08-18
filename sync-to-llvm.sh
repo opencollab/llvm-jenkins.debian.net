6433a46e3bb164132ef7ceae2e71a5f3b8c7068e
Sylvestre Ledru <sylvestre@debian.org>
2017-08-18 08:57:12 +0200

    delete the old dir + add comments

--- sync-to-llvm.sh
+++ sync-to-llvm.sh
@@ -16,8 +16,15 @@ if test ! -d $BASE_LOCALDIR/$REPOSITORY; then
 fi
 find $BASE_LOCALDIR -type d | xargs chmod 755
 find $BASE_LOCALDIR -type f ! -name sync-to-llvm.sh | xargs chmod 644
+echo "Delete potential old directory"
+time ssh $TARGET rm -rf $BASE_TARGETDIR/$REPOSITORY.back
+echo "Copy the current repo to a new directory to be updated"
 time ssh $TARGET cp -R $BASE_TARGETDIR/$REPOSITORY $BASE_TARGETDIR/$REPOSITORY.back
+echo "Sync the data"
 time /usr/bin/rsync --delay-updates --info=progress2 --block-size=131072 --protocol=29 --times --delete -v --stats -r $BASE_LOCALDIR/$REPOSITORY/* $TARGET:$BASE_TARGETDIR/$REPOSITORY.back/
+echo "Kill the current repo (by renaming it)"
 time ssh $TARGET mv $BASE_TARGETDIR/$REPOSITORY $BASE_TARGETDIR/$REPOSITORY.1
+echo "Move the new repo to the actual dir"
 time ssh $TARGET mv $BASE_TARGETDIR/$REPOSITORY.back $BASE_TARGETDIR/$REPOSITORY
+echo "Delete the old repo"
 time ssh $TARGET rm -rf $BASE_TARGETDIR/$REPOSITORY.1
