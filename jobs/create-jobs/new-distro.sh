5cceb75da5fd72385a0539b91c11e456eaf82caa
Sylvestre Ledru <sylvestre@debian.org>
2020-04-04 17:55:09 +0200

    new distro

--- jobs/create-jobs/new-distro.sh
+++ jobs/create-jobs/new-distro.sh
@@ -4,9 +4,9 @@ if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
-NAME="eoan"
+NAME="focal"
 
-VERSIONS=( 8 9 snapshot)
+VERSIONS=(  9 10 snapshot)
 for v in "${VERSIONS[@]}"
 do
         echo $v
