1ba1687f2a12f070217988f57ff9450f784813ce
Sylvestre Ledru <sylvestre@debian.org>
2021-03-14 18:11:13 +0100

    sync-form-other-arch.sh: improve the error support

--- sync-form-other-arch.sh
+++ sync-form-other-arch.sh
@@ -10,8 +10,16 @@ fi
 DISTRO=$1
 HOST=$2
 rsync -avzh jenkins@$HOST:/srv/repository/$DISTRO/ /tmp/tmp-$DISTRO/
-for f in /srv/repository/$DISTRO/dists/llvm-*; do
+if ! test -d /tmp/tmp-$DISTRO/srv/repository/$DISTRO/dists/; then
+	echo "Distro $DISTRO not existing yet"
+	exit 0
+fi
+for f in  /tmp/tmp-$DISTRO/srv/repository/$DISTRO/dists/llvm-*; do
 	VERSION=$(echo $f|sed -e "s|/srv/repository/.*/dists/llvm-toolchain-$DISTRO-\(.*\)|\1|g")
+	if test -z $VERSION; then
+		echo "VERSION not found"
+		exit 0
+	fi
 	reprepro -Vb /srv/repository/$DISTRO/ includedeb llvm-toolchain-$DISTRO-$VERSION /tmp/tmp-$DISTRO/pool/main/l/llvm-toolchain-$VERSION/*deb
 done
 if test -d /tmp/tmp-$DISTRO/pool/main/l/llvm-toolchain/; then
