cdc802a8cddc4fa1319e96f40cbaad805f74a8ba
Sylvestre Ledru <sylvestre@debian.org>
2021-03-14 21:52:31 +0100

    improve the import script

--- sync-form-other-arch.sh
+++ sync-form-other-arch.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 set -e
 
-if test $# -ne 2; then
+if test $# -ne 3; then
     echo "error"
-    echo "syntax $0 DISTRO HOST"
+    echo "syntax $0 DISTRO HOST ARCH"
     exit 1
 fi
 
 DISTRO=$1
 HOST=$2
+ARCH=$3
 rsync -avzh --delete jenkins@$HOST:/srv/repository/$DISTRO/ /tmp/tmp-$DISTRO/
 if ! test -d /tmp/tmp-$DISTRO/pool/main/; then
         echo "Distro $DISTRO not existing yet"
         exit 0
 fi
 
-for f in /tmp/tmp-$DISTRO/dists/llvm-*; do
+for f in /tmp/tmp-$DISTRO/dists/llvm-*/main/binary-$ARCH/; do
 	echo $f
-	VERSION=$(echo $f|sed -e "s|/tmp/tmp-$DISTRO/dists/llvm-toolchain-$DISTRO-\(.*\)|\1|g")
+	VERSION=$(echo $f|sed -e "s|/tmp/tmp-$DISTRO/dists/llvm-toolchain-$DISTRO-\(.*\)/.*|\1|g")
 	echo "VERSION $VERSION"
         if test -z $VERSION; then
                 echo "VERSION not found"
