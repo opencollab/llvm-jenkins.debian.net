05fecaf3e4ec0283d55d502a559b3c0707932814
Sylvestre Ledru <sylvestre@debian.org>
2021-04-10 10:01:24 +0200

    fix the import script for ubuntu s390x

--- sync-form-other-arch.sh
+++ sync-form-other-arch.sh
@@ -26,6 +26,7 @@ for f in /tmp/tmp-$DISTRO/dists/llvm-*/main/binary-$ARCH/; do
             VERSION=$(echo $f|sed -e "s|/tmp/tmp-$DISTRO/dists/llvm-toolchain-\([[:digit:]]\+\)/.*|\1|g")
             if ! [[ $VERSION =~ $re ]] ; then
                 echo "Probaby the nightly version"
+		break
             fi
 	fi
     # Import of the stable and stabilisation version
@@ -37,10 +38,10 @@ for f in /tmp/tmp-$DISTRO/dists/llvm-*/main/binary-$ARCH/; do
 done
 
 # Import of the nightly builds
-if test -d /tmp/tmp-$DISTRO/pool/main/l/llvm-toolchain/; then
+if test -d /tmp/tmp-$DISTRO/pool/main/l/llvm-toolchain/ -o -d /tmp/tmp-$DISTRO/pool/main/l/llvm-toolchain-snapshot/; then
     if test $DISTRO == "unstable"; then
 	    reprepro -Vb /srv/repository/$DISTRO/ includedeb llvm-toolchain /tmp/tmp-$DISTRO/pool/main/l/llvm-toolchain/*deb
     else
-        reprepro -Vb /srv/repository/$DISTRO/ includedeb llvm-toolchain-$DISTRO /tmp/tmp-$DISTRO/pool/main/l/llvm-toolchain-$DISTRO/*deb
+        reprepro -Vb /srv/repository/$DISTRO/ includedeb llvm-toolchain-$DISTRO /tmp/tmp-$DISTRO/pool/main/l/llvm-toolchain-snapshot/*deb
     fi
 fi
