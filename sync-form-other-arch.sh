686f46948d65b3c94b93881a5c0407887b6608f1
Sylvestre Ledru <sylvestre@debian.org>
2021-04-19 18:46:01 +0200

    check if the directory exists before the import

--- sync-form-other-arch.sh
+++ sync-form-other-arch.sh
@@ -40,7 +40,10 @@ done
 # Import of the nightly builds
 if test -d /tmp/tmp-$DISTRO/pool/main/l/llvm-toolchain/ -o -d /tmp/tmp-$DISTRO/pool/main/l/llvm-toolchain-snapshot/; then
     if test $DISTRO == "unstable"; then
-        reprepro -Vb /srv/repository/$DISTRO/ includedeb llvm-toolchain /tmp/tmp-$DISTRO/pool/main/l/llvm-toolchain/*deb
+        if test -d /tmp/tmp-$DISTRO/pool/main/l/llvm-toolchain/; then
+            # In case snapshot hasn't been built yet
+            reprepro -Vb /srv/repository/$DISTRO/ includedeb llvm-toolchain /tmp/tmp-$DISTRO/pool/main/l/llvm-toolchain/*deb
+        fi
     else
         reprepro -Vb /srv/repository/$DISTRO/ includedeb llvm-toolchain-$DISTRO /tmp/tmp-$DISTRO/pool/main/l/llvm-toolchain-snapshot/*deb
     fi
