13bbbdb52275c49bbf89208fae075e6d54e53135
Sylvestre Ledru <sylvestre@debian.org>
2021-07-06 22:37:38 +0200

    rekor: strip a binary

--- build-rekor.sh
+++ build-rekor.sh
@@ -32,6 +32,7 @@ git clone https://github.com/sigstore/rekor.git rekor-cli
 go get -u -t -v github.com/sigstore/rekor/cmd/rekor-cli
 cd \$GOPATH/src/github.com/sigstore/rekor/cmd/rekor-cli
 go build -v -o rekor
+strip /root/go/src/github.com/sigstore/rekor/cmd/rekor-cli/rekor
 GLOBALEOF
 
 chroot $PATH_CHROOT/ bash ./root/run.sh
