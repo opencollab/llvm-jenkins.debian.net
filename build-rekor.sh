c61091ede328143e23e1fb89d1ec9c5f9adcbc9b
Sylvestre Ledru <sylvestre@debian.org>
2021-07-06 21:25:42 +0200

    build-rekor: add a retry and various fixes

--- build-rekor.sh
+++ build-rekor.sh
@@ -17,17 +17,16 @@ if test ! -e $0.chroot/dev/pts; then
         mount --bind /dev/pts "$d.chroot/dev/pts" || true
 fi
 
-if test ! -d PATH_CHROOT; then
-	echo "chroot $PATH_CHROOT not found"
-    exit 1
-fi
-
-cat > PATH_CHROOT/root/run.sh <<GLOBALEOF
+cat > $PATH_CHROOT/root/run.sh <<GLOBALEOF
 set -e
 set -v
 
 apt install -y golang git
-go get -u -t -v github.com/sigstore/rekor/cmd/rekor-cli
+while ! go get -u -t -v github.com/sigstore/rekor/cmd/rekor-cli
+do
+  echo "Try again"
+done
+
 cd $GOPATH/src/github.com/sigstore/rekor/cmd/rekor-cli
 go build -v -o rekor
 cp rekor /usr/local/bin/
@@ -35,5 +34,5 @@ GLOBALEOF
 
 chroot $PATH_CHROOT/ bash ./root/run.sh
 
-cp PATH_CHROOT/root/go/src/github.com/sigstore/rekor/cmd/rekor-cli/rekor .
+cp $PATH_CHROOT/root/go/src/github.com/sigstore/rekor/cmd/rekor-cli/rekor .
 ./rekor version
