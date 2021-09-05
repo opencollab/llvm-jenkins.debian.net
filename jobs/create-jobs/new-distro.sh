81630342f4f7f33969fde313c2b4e2a1a8823a5a
Sylvestre Ledru <sylvestre@debian.org>
2021-09-05 17:52:43 +0200

    prepare the new ubuntu version

--- jobs/create-jobs/new-distro.sh
+++ jobs/create-jobs/new-distro.sh
@@ -4,9 +4,9 @@ if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
-NAME="bullseye"
+NAME="impish"
 
-VERSIONS=(  11 12 snapshot)
+VERSIONS=(12 13 snapshot)
 for v in "${VERSIONS[@]}"
 do
         echo $v
