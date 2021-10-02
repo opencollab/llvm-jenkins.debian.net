9518ff87af0bf4586264d6cc344e6f676c7dce03
Sylvestre Ledru <sylvestre@debian.org>
2021-10-02 13:34:11 +0200

    Comment the script

--- image-update.sh
+++ image-update.sh
@@ -4,18 +4,27 @@ set -e
 # Update the gcloud image
 gcloud compute instances list
 
+# Start the VM
 gcloud compute instances start debian-build-node &> out.log
+
+# Retrieve the public ip
 IP=$(grep "external" out.log|awk '{print $5}')
+echo "Sleep until $IP is live"
 sleep 30s
 ssh $IP
+
 # hack on the vm
+
+# Stop the vm for the image creation
 gcloud compute instances stop debian-build-node
 
+# Retrieve the current image
 gcloud compute images list --filter="name~'image-debian-node-.*'" &> out.log
 NODEID=$(grep "image-debian-node" out.log|awk '{print $1}'||cut -d- -f4)
 NEW_NODE=i=$((NODEID+1))
-# TODO increment
+# Create the new image
 gcloud compute images create image-debian-node-$NEW_NODE --source-disk=debian-build-node
+# Obsolete the old one
 gcloud compute images deprecate  image-debian-node-$NODEID --state=OBSOLETE --replacement=image-debian-node-$NEW_NODE
 
 echo "Update jenkins on http://URL/configureClouds/"
