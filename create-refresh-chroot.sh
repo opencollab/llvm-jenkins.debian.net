fbdacae174946dc4c1e45052a9011cec7d2e8bb5
Sylvestre Ledru <sylvestre@debian.org>
2021-10-10 11:43:44 +0200

    change the install path for the config file

--- create-refresh-chroot.sh
+++ create-refresh-chroot.sh
@@ -2,7 +2,7 @@
 ARCHS="amd64 i386"
 DEBIAN_DISTRO="buster bullseye unstable"
 UBUNTU_DISTRO="bionic focal groovy hirsute impish"
-cat > configfile  <<EOF
+cat > /tmp/configfile  <<EOF
 COMPONENTS="main universe"
 EOF
 
@@ -24,6 +24,8 @@ for d in $UBUNTU_DISTRO; do
     if test -d /var/cache/pbuilder/base-$d-$a.cow; then
         sudo DIST=$d ARCH=$a cowbuilder --update --basepath /var/cache/pbuilder/base-$d-$a.cow
     else
-        sudo DIST=$d ARCH=$a cowbuilder --create --basepath /var/cache/pbuilder/base-$d-$a.cow --distribution $d --debootstrap debootstrap --architecture $a --debootstrapopts --arch --debootstrapopts amd64 --debootstrapopts --variant=buildd --configfile=configfile --hookdir /usr/share/jenkins-debian-glue/pbuilder-hookdir/
+        sudo DIST=$d ARCH=$a cowbuilder --create --basepath /var/cache/pbuilder/base-$d-$a.cow --distribution $d --debootstrap debootstrap --architecture $a --debootstrapopts --arch --debootstrapopts amd64 --debootstrapopts --variant=buildd --configfile=/tmp/configfile --hookdir /usr/share/jenkins-debian-glue/pbuilder-hookdir/
     fi
 done
+rm -f /tmp/configfile
+
