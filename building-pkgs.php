d0b67a468e8c251173e197bab182cd7541ee3088
Sylvestre Ledru <sylvestre@debian.org>
2022-01-27 19:05:37 +0100

    refresh the build doc

--- building-pkgs.php
+++ building-pkgs.php
@@ -31,14 +31,14 @@ Building the latest nightly snapshot
   </p>
 
   <p>This should download all the original snapshot tarballs, and create a
-  directory named llvm-toolchain-snapshot-3.9~svn270412. Depending on the last
-  update of the jenkins nightly builder, the snapshot version number and svn
-  release will vary.</p>
+  directory named llvm-toolchain-snapshot-X.Y~++202.... Depending on the last
+  update of the jenkins nightly builder, the snapshot version number and git
+  hash will vary.</p>
 
   <p>Then install the build dependencies,</p>
   <p class="www_code"> $ sudo apt-get build-dep llvm-toolchain-snapshot </p>
 
-  <p> On older versions of Ubuntu, some build dependencies cannot be satisfied
+  <p>On older versions of Ubuntu, some build dependencies cannot be satisfied
   because the required gcc versions are missing. To fix this issue you should add
   the <a href="https://launchpad.net/~ubuntu-toolchain-r/+archive/ubuntu/ppa">Ubuntu
   Toolchain PPA</a> to your system before executing the above command.</p>
@@ -46,7 +46,7 @@ Building the latest nightly snapshot
   <p>Once everything is ready, enter the directory and build the package,</p>
 
   <p class="www_code">
-    $ cd llvm-toolchain-snapshot-3.9~svn270412/ <br />
+    $ cd llvm-toolchain-snapshot-X.Y~++202*/ <br />
     $ debuild -us -uc -b
   </p>
 </div>
@@ -71,43 +71,42 @@ Building a snapshot package by hand
       <p>Run the orig-tar.sh script,</p>
       <p class="www_code">$ sh snapshot/debian/orig-tar.sh</p>
       <p>which will retrieve the latest version for each LLVM subproject
-        (llvm, clang, lldb, etc.) from the main development SVN and repack
+        (llvm, clang, lldb, etc.) from the git repository and repack
         it as a set of tarballs.</p></li>
     <li><p>Unpack the original tarballs and apply quilt Debian patches.</p>
       <p>run the unpack.sh script,</p>
       <p class="www_code">$ sh snapshot/debian/unpack.sh</p>
       <p>which will unpack the source tree inside a new directory such as
-        <i>branches/llvm-toolchain-snapshot_3.9~svn268942</i>.
-        Depending on the current snapshot version number and svn release,
+        <i>branches/llvm-toolchain-snapshot_X.Y~++200*</i>.
+        Depending on the current snapshot version number and git hash,
         the directory name will be different. Quilt patches will then be
         applied.</p></li>
     <li><p>Build the binary packages using,</p>
-      <p class="www_code">$ fakeroot debian/rules binary</p>
+      <p class="www_code">$ debian/rules binary</p>
       <p>When debugging, successive builds can be recompiled faster by using tools
-        such as ccache (PATH=/usr/lib/ccache:$PATH fakeroot debian/rules
+        such as ccache (PATH=/usr/lib/ccache:$PATH debian/rules
         binary).</p></li>
   </ol>
 
   <h2>Retrieving a specific branch or release candidate with orig-tar.sh</h2>
 
   <p>When using orig-tar.sh, if you need to retrieve a specific branch, you can
-    pass the branch name as the first argument. For example, to get the 3.8 release
+    pass the branch name as the first argument. For example, to get the 13 release
     branch at
-    <i>http://llvm.org/svn/llvm-project/{llvm,...}/branches/release_38</i> you
+    <i>https://github.com/llvm/llvm-project/tree/release/13.x</i> you
     should use,</p>
-  <p class="www_code">$ sh 3.8/debian/orig-tar.sh release_38</p>
+  <p class="www_code">$ sh 13/debian/orig-tar.sh release/13.x</p>
   <p>To retrieve a specific release candidate, you can pass the branch name as the
     first argument, and the tag rc number as the second argument. For example, to
-    get the 3.8.0 release candidate rc3 at
-    <i>http://llvm.org/svn/llvm-project/{llvm,...}/tags/RELEASE_380/rc3</i>
+    get the 13.0.1 release candidate rc3 at
+    <i>https://github.com/llvm/llvm-project/releases/tag/llvmorg-13.0.1-rc3</i>
     you should use,</p>
 
-  <p class="www_code">$ sh 3.8/debian/orig-tar.sh RELEASE_380 rc3</p>
+  <p class="www_code">$ sh 13/debian/orig-tar.sh 13.0.1 rc3</p>
 
   <h2>Organization of the repository</h2>
   <p>The Debian package for each LLVM point release is maintained as a separate
-    SVN branch in the branches/ directory. For example, the 3.8 release lives at
-    branches/3.8.</p>
+    git branch. For example, the 3.8 release lives at 3.8.</p>
   <p>The current snapshot release is maintained at branches/snapshot.</p>  <h2>Organization of the repository</h2>
 
   <h2>Clone all branches</h2>
@@ -120,7 +119,7 @@ Building a snapshot package by hand
   </p>
 
   <h2>Additional maintainer scripts</h2>
-  <p>The script <i>qualify-clang.sh</i> that is found at the SVN root should
+  <p>The script <i>qualify-clang.sh</i> that is found at the root should
     be used to quickly test a newly built clang package. It runs a short set
     of sanity-check tests.</p>
   <p>The script <i>releases/snapshot/debian/prepare-new-release.sh</i> is used when
@@ -128,12 +127,6 @@ Building a snapshot package by hand
     various files of the package.</p>
 </div>
 
-<!--
-Changes:
-
-- May 25th 2016:
-  * Initial version
--->
 <p style="font-size: smaller;">
      Contact: <a href="mailto:sylvestre@debian.org">Sylvestre Ledru</a>
 <br />Build infra by <a href="http://www.irill.org/">IRILL</a> / Hosting by LLVM Foundation
