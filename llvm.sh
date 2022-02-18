a3cda3e9986f7436ac620c3982404aba9299be2e
Sylvestre Ledru <sylvestre@debian.org>
2022-02-18 11:16:57 +0100

    llvm.sh: add a missing space

--- llvm.sh
+++ llvm.sh
@@ -86,7 +86,7 @@ case "$DIST_VERSION" in
     Ubuntu_20.04 )   REPO_NAME="deb http://apt.llvm.org/focal/     llvm-toolchain-focal$LLVM_VERSION_STRING   main" ;;
     Ubuntu_20.10 )   REPO_NAME="deb http://apt.llvm.org/groovy/    llvm-toolchain-groovy$LLVM_VERSION_STRING  main" ;;
     Ubuntu_21.04 )   REPO_NAME="deb http://apt.llvm.org/hirsute/   llvm-toolchain-hirsute$LLVM_VERSION_STRING main" ;;
-    Ubuntu_21.10 )   REPO_NAME="deb http://apt.llvm.org/impish/   llvm-toolchain-impish$LLVM_VERSION_STRING main" ;;
+    Ubuntu_21.10 )   REPO_NAME="deb http://apt.llvm.org/impish/    llvm-toolchain-impish$LLVM_VERSION_STRING main" ;;
 
     Linuxmint_19* )  REPO_NAME="deb http://apt.llvm.org/bionic/   llvm-toolchain-bionic$LLVM_VERSION_STRING  main" ;;
     Linuxmint_20* )  REPO_NAME="deb http://apt.llvm.org/focal/    llvm-toolchain-focal$LLVM_VERSION_STRING   main" ;;
