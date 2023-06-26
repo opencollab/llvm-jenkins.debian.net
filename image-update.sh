364ad9223f3077a571af6ffd47c661bab2bf799c
Sylvestre Ledru <sylvestre@debian.org>
2023-06-26 15:29:35 +0200

    update of the url

--- image-update.sh
+++ image-update.sh
@@ -42,7 +42,7 @@ gcloud compute images create  image-debian-node-$NEW_NODE --source-disk=debian-b
 # Obsolete the old one
 gcloud compute images deprecate image-debian-node-$NODEID --state=OBSOLETE --replacement=image-debian-node-$NEW_NODE
 
-echo "Update jenkins on https://llvm-jenkins.debian.net/configureClouds/"
+echo "Update jenkins on https://llvm-jenkins.debian.net/manage/cloud/gce-gce/configure"
 echo "Delete the old image (image-debian-node-$NODEID) once image-debian-node-$NEW_NODE is OK"
 echo "https://console.cloud.google.com/compute/images?tab=images&authuser=1&"
 rm out.log
