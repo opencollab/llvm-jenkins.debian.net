6512f7948dd356146b3bf5511473c0177e06dc3e
Sylvestre Ledru <sylvestre@debian.org>
2024-01-24 14:04:44 +0000

    improve the new version script

--- jobs/create-jobs/new-version.sh
+++ jobs/create-jobs/new-version.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
-VERSION="17"
-VERSION_2="170"
-DISTROS=( unstable buster bullseye bookworm bionic focal lunar jammy kinetic )
+#kinetic!/bin/bash
+VERSION="18"
+VERSION_2="180"
+DISTROS=( unstable buster bullseye bookworm bionic focal lunar jammy mantic )
 for d in "${DISTROS[@]}"
 do
 	echo $d
@@ -14,7 +14,7 @@ do
 	fi
 	echo "sed -i -e 's|llvm-toolchain$d-binaries,|llvm-toolchain$d-binaries, llvm-toolchain$d-$VERSION-binaries,|' ../llvm-toolchain$d-binaries-sync/config.xml"
 done
-echo "update the sync job to upload the version (by hand in the interface)"
+echo "update the sync job to upload the version (by hand in the interface) - links at the end"
 echo "Disable i386 on recent versions of Ubuntu"
 echo "Disable the old version when ready"
 D=$(echo "{$DISTROS}"| sed -e "s| |,|g")
@@ -33,5 +33,21 @@ jenkins.instance.getView("13").items.each { item ->
     item.disabled = true
 }
 EOF
-
 echo "in https://llvm-jenkins.debian.net/script"
+
+for d in "${DISTROS[@]}"
+do
+    # sh create-new-job.sh unstable 3.5 release_35 3.5
+    if test "$d" == "unstable"; then
+        d=""
+    else
+        d="-$d"
+    fi
+    echo "https://llvm-jenkins.debian.net/view/sync/job/llvm-toolchain$d-binaries-sync/configure"
+done
+echo -n "emacs "
+for d in "${DISTROS[@]}"
+do
+    echo -n "/srv/repository/$d/conf/distributions "
+done
+echo ""
