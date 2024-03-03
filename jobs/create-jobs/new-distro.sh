9d5443fb83b22b96ea60fcb227db30f838b1d3d5
Sylvestre Ledru <sylvestre@debian.org>
2024-03-03 11:14:49 +0000

    automate more the new distro process

--- jobs/create-jobs/new-distro.sh
+++ jobs/create-jobs/new-distro.sh
@@ -4,11 +4,11 @@ if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
-NAME="mantic"
+NAME="noble"
 
 IS_UBUNTU=1
 
-VERSIONS=(16 17 snapshot)
+VERSIONS=(17 18 snapshot)
 for v in "${VERSIONS[@]}"
 do
         echo $v
@@ -26,30 +26,41 @@ cd /usr/share/debootstrap/scripts
 echo "Make sure that the version for  /usr/share/debootstrap/scripts is correct"
 echo "EDIT ME to verify"
 ln -s trusty $NAME
+cd -
 
 mkdir  -p /srv/repository/$NAME
 chown jenkins. /srv/repository/$NAME
 
-emacs ~jenkins/llvm-jenkins.debian.net.git/pbuilderrc
-echo "commit ~jenkins/pbuilderrc"
+cd ~jenkins/llvm-jenkins.debian.net.git/
+sed -i "s/UBUNTU_SUITES=(/UBUNTU_SUITES=(\"$NAME\" /" pbuilderrc
+git commit -m "add $NAME in pbuilderrc" pbuilderrc
+sed -i -e 's/\(DISTRO=".*\)"/\1 $NAME"/' test-install.sh
+git commit -m "add $NAME in test-install.sh" test-install.sh
+ed -i -e 's/\(UBUNTU_DISTRO=".*\)"/\1 $NAME"/' create-refresh-image.sh
+it commit -m "add $NAME in create-refresh-image.sh" create-refresh-image.sh
+cd -
+
+rm -rf llvm-toolchain-$NAME-source-trigger
+sed -i -e 's|  <triggers/>|  <triggers>\n    <hudson.triggers.TimerTrigger>\n      <spec>H 8,20 * * *</spec>\n    </hudson.triggers.TimerTrigger>\n  </triggers>|' llvm-toolchain-$NAME-source/config.xml
+
+sed -i -e "s|@STABLE@|${VERSIONS[0]}|" -e "s|@DEV@|${VERSIONS[1]}|" llvm-toolchain-$NAME-binaries-sync/config.xml
+
+sed -i -e "s|release/${VERSIONS[0]}|release/${VERSIONS[0]}.x|" llvm-toolchain-$NAME-${VERSIONS[0]}-source/config.xml
+sed -i -e "s|release/${VERSIONS[1]}|release/${VERSIONS[1]}.x|" llvm-toolchain-$NAME-${VERSIONS[1]}-source/config.xml
+
 echo "On every slave, git pull + create the symlink from $NAME for deboostrap"
 echo "including other archs"
-echo "remove i386 in case of ubuntu distro"
-echo "also delete llvm-toolchain-$NAME-source-trigger and update the cron in the job"
 echo "Add the new version in /srv/salt/llvm-slave.sls on cocoro for /usr/share/debootstrap/scripts"
 echo "run with salt 'llvm*' state.apply"
-echo "Update llvm-toolchain-$NAME-binaries-sync to fix the version to sync"
 echo "Update the update of the build in the main job llvm-toolchain-$NAME-source"
-echo "Update the version (not snapshot) to add the .x of the branch name in the orig-tar.sh script (ex: release/12.x instead of release/12)"
-echo "in /srv/repository/$NAME/conf/* maybe copy the configuration from other repo to avoid s390x issues"
-echo "ex: cp /srv/repository/jammy/conf/distributions /srv/repository/$NAME/conf/"
-echo "create srv/repository/$NAME/conf/distributions on arm64 & s390x:"
+echo "run the following commands":
+echo "PREVIOUS=UPDATE"
+echo "cp -R /srv/repository/\$PREVIOUS/conf /srv/repository/$NAME/conf/"
+echo "sed -i -e 's|\$PREVIOUS|$NAME|g' /srv/repository/$NAME/conf/distributions"
 echo "ssh jenkins@cb0dd220.packethost.net mkdir -p /srv/repository/$NAME/conf/"
 echo "ssh llvm-jenkins-s390x-1.debian.net mkdir -p /srv/repository/$NAME/conf/"
 echo "scp /srv/repository/$NAME/conf/distributions jenkins@cb0dd220.packethost.net:/srv/repository/$NAME/conf/distributions"
 echo "scp /srv/repository/$NAME/conf/distributions llvm-jenkins-s390x-1.debian.net:/srv/repository/kinetic/conf/distributions"
-echo " emacs test-install.sh create-refresh-image.sh  + commit"
-echo "Run image-update.sh"
+echo "Run image-update.sh on your system"
 echo "create the filter view"
 echo "start the jobs in jenkins"
-echo "add it into test-install.sh"
