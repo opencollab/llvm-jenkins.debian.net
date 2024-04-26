0180c7782611047be5084a843b78e46cded90741
kasperk81 <83082615+kasperk81@users.noreply.github.com>
2024-04-26 17:53:19 +0300

    make llvm.sh portable

--- llvm.sh
+++ llvm.sh
@@ -27,13 +27,17 @@ BASE_URL="http://apt.llvm.org"
 needed_binaries=(lsb_release wget add-apt-repository gpg)
 missing_binaries=()
 for binary in "${needed_binaries[@]}"; do
-    if ! which $binary &>/dev/null ; then
+    if ! command -v $binary &>/dev/null ; then
+        if [[ "$binary" == "wget" ]] && command -v curl &>/dev/null; then
+            using_curl=1
+            continue
+        fi
         missing_binaries+=($binary)
     fi
 done
 if [[ ${#missing_binaries[@]} -gt 0 ]] ; then
     echo "You are missing some tools this script requires: ${missing_binaries[@]}"
-    echo "(hint: apt install lsb-release wget software-properties-common gnupg)"
+    echo "(hint: apt install lsb-release wget (or curl) software-properties-common gnupg)"
     exit 4
 fi
 
@@ -136,12 +140,24 @@ fi
 
 LLVM_VERSION_STRING=${LLVM_VERSION_PATTERNS[$LLVM_VERSION]}
 
+if [[ -z "$using_curl" ]]; then
+    download_tool=wget
+    quiet_flag="-q"
+    head_flag="--method=HEAD"
+    stdout_flag="-O-"
+else
+    download_tool=curl
+    quiet_flag="-s"
+    head_flag="-XHEAD"
+    default_flag="-SL"
+fi
+
 # join the repository name
 if [[ -n "${CODENAME}" ]]; then
     REPO_NAME="deb ${BASE_URL}/${CODENAME}/  llvm-toolchain${LINKNAME}${LLVM_VERSION_STRING} main"
 
     # check if the repository exists for the distro and version
-    if ! wget -q --method=HEAD ${BASE_URL}/${CODENAME} &> /dev/null; then
+    if ! "$download_tool" $default_flag $quiet_flag $head_flag ${BASE_URL}/${CODENAME} &> /dev/null; then
         if [[ -n "${CODENAME_FROM_ARGUMENTS}" ]]; then
             echo "Specified codename '${CODENAME}' is not supported by this script."
         else
@@ -156,7 +172,7 @@ fi
 
 if [[ ! -f /etc/apt/trusted.gpg.d/apt.llvm.org.asc ]]; then
     # download GPG key once
-    wget -qO- https://apt.llvm.org/llvm-snapshot.gpg.key | tee /etc/apt/trusted.gpg.d/apt.llvm.org.asc
+    "$download_tool" $default_flag $quiet_flag $stdout_flag https://apt.llvm.org/llvm-snapshot.gpg.key | tee /etc/apt/trusted.gpg.d/apt.llvm.org.asc
 fi
 
 if [[ -z "`apt-key list 2> /dev/null | grep -i llvm`" ]]; then
