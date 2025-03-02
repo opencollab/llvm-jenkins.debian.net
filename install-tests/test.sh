62effafc394efc13a80fe677ec82199b1d8bcce1
Sylvestre Ledru <sylvestre@debian.org>
2025-03-02 12:02:31 +0100

    refresh the list of version

--- install-tests/test.sh
+++ install-tests/test.sh
@@ -3,7 +3,7 @@
 # test the llvm.sh installation script for the different distros
 
 # LLVM versions to be tested
-LLVM_VERSIONS=(16 17 18)
+LLVM_VERSIONS=(18 19 20 21)
 
 # Linux distributions to be tested
 # the distro names must match to the name of a docker image!
