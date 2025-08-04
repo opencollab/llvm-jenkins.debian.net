18ce4f8270adca29e2fb8d598879668da57d7306
Sylvestre Ledru <sylvestre@debian.org>
2025-08-04 12:22:16 +0200

    22 is now dev

--- index.php
+++ index.php
@@ -42,9 +42,9 @@ function getLastRevision($distro) {
     return null; // Return null if the version line isn't found
 }
 
-$stableBranch="19";
-$qualificationBranch="20";
-$devBranch="21";
+$stableBranch="20";
+$qualificationBranch="21";
+$devBranch="22";
 $isQualification=true;
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
